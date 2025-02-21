ad7097fdc3618c786ffa039ec45cb7572c6c2fe9
Alex Russell <alexrussellria@gmail.com>
2025-02-21 15:01:50 -0330

    Added more data to TrackingSystem class

--- MedicationTrackingSystem.java
+++ MedicationTrackingSystem.java
@@ -22,11 +22,47 @@ public class MedicationTrackingSystem {
         Patient patient2 = new Patient(22, "Jill", 25, "709-333-2222");
         patients.add(patient2);
 
-        Doctor doctor1 = new Doctor(20, "Gordon", 65, "709-444-4444", "Cardiology");
+        Patient patient3 = new Patient(23, "Jack", 35, "709-444-2222");
+        patients.add(patient3);
+
+        Patient patient4 = new Patient(24, "Gill", 25, "709-555-2222");
+        patients.add(patient4);
+
+        Doctor doctor1 = new Doctor(1, "Gordon", 65, "709-444-4444", "Cardiology");
         doctors.add(doctor1);
 
-        Doctor doctor2 = new Doctor(20, "Gord", 65, "709-555-5555", "Cancer");
+        Doctor doctor2 = new Doctor(2, "Gord", 65, "709-555-5555", "Cancer");
         doctors.add(doctor2);
+
+        Doctor doctor3 = new Doctor(3, "Gordy", 65, "709-666-6666", "Pediatrics");
+        doctors.add(doctor3);
+
+        Medication med1 = new Medication(1, "Tylenol", "10mg", 5);
+        medications.add(med1);
+
+        Medication med2 = new Medication(2, "Advil", "20mg", 10);
+        medications.add(med2);
+
+        Medication med3 = new Medication(3, "Marijuana", "3.5g", 5);
+        medications.add(med3);
+
+        Medication med4 = new Medication(4, "Morphine", "5mg", 5);
+        medications.add(med4);
+
+        Prescription script1 = new Prescription(doctor1, patient1, med1);
+        prescriptions.add(script1);
+
+        Prescription script2 = new Prescription(doctor2, patient2, med2);
+        prescriptions.add(script2);
+
+        Prescription script3 = new Prescription(doctor3, patient3, med3);
+        prescriptions.add(script3);
+
+        Prescription script4 = new Prescription(doctor1, patient4, med3);
+        prescriptions.add(script4);
+
+        Prescription script5 = new Prescription(doctor2, patient1, med3);
+        prescriptions.add(script5);
     }
 
     public void addPatient(Patient patient) {
