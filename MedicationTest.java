362af0abc1234ad357d0a5af5e61faec34b415a4
Abdul Rahman Zahiri <abdulrahmanzahiri6644@gmail.com>
2025-03-01 14:06:21 -0330

    Deleted te test

--- MedicationTest.java
+++
@@ -1,9 +0,0 @@
-
-public class MedicationTest {
-    public static void main(String[] args) {
-       Medication med1 = new Medication(1, "Acetaminophen", "250mg", 200);
-
-
-       System.out.println(med1);
-    }
-}
